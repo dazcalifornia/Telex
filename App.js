3ce8ecfde282d84b43ebf37b4f09008acc8c6c77
Franx <divertzt@gmail.com>
2022-06-06 03:36:55 +0700

    update app.js

--- App.js
+++ App.js
@@ -1,112 +1,84 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
+  StyleSheet, 
+  Text, 
+  View, 
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  Platform,
+  Image,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="SUCC Damn">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+import Test from './components/Test';
+export default function App() {
+   return(
+     <SafeAreaView style={styles.container}>
+        <View style={styles.Header}>
+                <View style={styles.headerDetails}>
+                  <Image source={{uri:'https://source.unsplash.com/user/imransuleiman'}} style={styles.image}/>
+                    <Text style={styles.text}>Hello</Text>
+                    <Text style={styles.name}>@username</Text>
+                </View>
+                <Text>setting</Text>
         </View>
-      </ScrollView>
+        <View style={styles.box}>
+            <Text style={[styles.text, {color:'#263238',textAlign:'left'}]}>Friend</Text>
+        </View>
+        <View style={styles.menu}>
+          <Text>All friend</Text>
+          <Text>Closed friend</Text>
+          </View>
     </SafeAreaView>
-  );
-};
-
+   );
+}
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+   container: {
+      flexDirection:'column',
+     flex: 1,
+     alignItems: 'center',
+     justifyContent: 'flex-start',
+     blackgroundColor:'#322A26',
+     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+   },
+   Header:{
+    borderColor:'#02051f',
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:'center',
+    padding:10,
+    width:'100%',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  headerDetails:{
+      flex:1,
+      flexDirection:'column',
+      paddingTop:10,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  name:{
+      color:'#9CA3DB',
+      textAlign:'center',
+      fontSize: 20,
+      fontweight:'bold',
+      padding:10,
   },
-  highlight: {
-    fontWeight: '700',
+  text:{
+      color:'#9CA3DB',
+      textAlign:'center',
+      fontSize:35,
+      fontweight:'bold',
+      padding:10,
+  },
+  image:{
+      width: 75,
+      height: 75,
+      borderRadius: 75/2,
+  },
+  box: {
+    justifyContent: 'center',
+    width: '100%',
+    height: 50,
+  },
+  menu: {
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    padding: 15,
   },
 });
-
-export default App;
+ 
